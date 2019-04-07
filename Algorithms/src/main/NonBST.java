9c8cf7ec02132df517bedb68731d8d9d13e387b4
Jkavia <Jkavia@github.com>
2019-04-06 23:50:50 -0400

    commit

--- Algorithms/src/main/NonBST.java
+++ Algorithms/src/main/NonBST.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -14,11 +15,60 @@ public class NonBST {
 		addnode(root, 6);
 		addnode(root, 7);
 		addnode(root, 8);
-		
-		
-		//printTree(root);
-		//System.out.println("val is "+commonancestor(root,8,5).value);
 
+		// printTree(root);
+		// System.out.println("val is "+commonancestor(root,8,5).value);
+
+	}
+
+	/** given a binary tree print all possible arrays that have led to this tree **/
+	ArrayList<LinkedList<Integer>> allSequences(Treenode node) {
+		ArrayList<LinkedList<Integer>> result = new ArrayList<LinkedList<Integer>>();
+		if (node == null) {
+			result.add(new LinkedList<Integer>());
+			return result;
+		}
+		LinkedList<Integer> prefix = new LinkedList<Integer>();
+		prefix.add(node.value);
+
+		ArrayList<LinkedList<Integer>> leftseq = allSequences(node.left);
+		ArrayList<LinkedList<Integer>> rightseq = allSequences(node.right);
+
+		for (LinkedList<Integer> left : leftseq) {
+			for (LinkedList<Integer> right : rightseq) {
+				ArrayList<LinkedList<Integer>> weaved = new ArrayList<>();
+				weaveLists(left, right, weaved, prefix);
+				result.addAll(weaved);
+			}
+		}
+
+		return result;
+
+	}
+
+	private void weaveLists(LinkedList<Integer> first, LinkedList<Integer> second, ArrayList<LinkedList<Integer>> results,
+			LinkedList<Integer> prefix) {
+	if(first.size()==0||second.size()==0) {
+		LinkedList<Integer> result = (LinkedList<Integer>)prefix.clone();
+		result.addAll(first);
+		result.addAll(second);
+		results.add(result);
+		return;
+	}
+		
+	
+	int headFirst = first.removeFirst();
+	prefix.addLast(headFirst);
+	weaveLists(first, second, results, prefix);
+	prefix.removeLast();
+	first.addFirst(headFirst);
+	
+	int headSecond = second.removeFirst();
+	prefix.addLast(headSecond);
+	weaveLists(first, second, results, prefix);
+	prefix.removeLast();
+	second.addFirst(headSecond);
+	
 	}
 
 	/** find the first common ancestor **/
@@ -50,7 +100,7 @@ public class NonBST {
 
 		if (rx.node != null && ry.node != null) {
 			return new Result(root, true);
-		} else if (p== root.value || q== root.value) {
+		} else if (p == root.value || q == root.value) {
 			boolean isAncestor = rx.node != null || ry.node != null;
 			return new Result(root, isAncestor);
 		} else {
@@ -91,10 +141,10 @@ public class NonBST {
 			return;
 		}
 		printTree(root.left);
-		System.out.print(root.value+" ");
+		System.out.print(root.value + " ");
 		printTree(root.right);
 	}
-	
+
 	/** Print the tree in post order **/
 	public static void printTreepost(Treenode root) {
 		if (root == null) {
@@ -102,7 +152,7 @@ public class NonBST {
 		}
 		printTree(root.left);
 		printTree(root.right);
-		System.out.print(root.value+" ");
+		System.out.print(root.value + " ");
 	}
 
 }
