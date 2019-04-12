b98831d5c03794fe2dd9d92df650670a0b7fb8d2
Jkavia <Jkavia@github.com>
2019-04-12 12:10:21 -0400

    commit

--- Algorithms/src/amazon/ThreeSum.java
+++ Algorithms/src/amazon/ThreeSum.java
@@ -12,7 +12,7 @@ public class ThreeSum {
 
 		int[] arr = {-1,0,1,2,-1,-4};
 
-		List<List<Integer>> ll=ThreeSum(arr);
+		List<List<Integer>> ll=ThreeSumMethod(arr);
 		for(List<Integer> al:ll) {
 			for(Integer i:al) {
 				System.out.print(i+" ");
@@ -21,8 +21,25 @@ public class ThreeSum {
 		}
 	}
 
-	public static List<List<Integer>> ThreeSum(int[] nums) {
-		
+	public static List<List<Integer>> ThreeSumMethod(int[] nums) {
+		Arrays.sort(nums);
+		List<List<Integer>> returnlist = new ArrayList<List<Integer>>();
+		for(int i=0;i<nums.length;i++) {
+			if(i>0 && nums[i]==nums[i-1]) {continue;}
+			int sum = -nums[i],left=i+1,right=nums.length-1;
+			
+			while(left<right) {
+				if(nums[left]+nums[right]==sum) {
+					returnlist.add(Arrays.asList(nums[left],nums[right],nums[i]));
+					left++;right--;
+					while(nums[left]==nums[left-1]) {left++;}
+					while(nums[right]==nums[right+1]) {right--;}
+					
+				}else if(nums[left]+nums[right]>sum) {left++;}else {right--;}
+			}
+			
+		}
+		return returnlist;
     }
 	}
 
