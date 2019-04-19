51d036e81cef3a25182e78d1ce74650d1450eae8
Jkavia <Jkavia@github.com>
2019-04-19 00:19:07 -0400

    commit

--- Algorithms/src/amazon/BestTimetoBuy.java
+++ Algorithms/src/amazon/BestTimetoBuy.java
@@ -3,7 +3,22 @@ package amazon;
 public class BestTimetoBuy {
 
 	public static void main(String[] ar) {
+		int arr[] = {9,1,2,4,1,5};
+		System.out.println(findMaximumProfit(arr));
+	}
+
+	private static int findMaximumProfit(int[] arr) {
+		int minvalue = arr[0], maxvalue=0;
+	for(int i=0;i<arr.length;i++) {
+		if(arr[i]>minvalue) {
+			maxvalue = Math.max(maxvalue,arr[i]-minvalue);
+		}else {
+			minvalue = arr[i];
+		}
+	}
+	
 		
+		return maxvalue;
 	}
 	
 }
