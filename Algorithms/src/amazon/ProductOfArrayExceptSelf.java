36e4412bfaa4f52eb217c997ed422c675a8ff847
Jkavia <Jkavia@github.com>
2019-04-27 23:23:03 -0400

    commit

--- Algorithms/src/amazon/ProductOfArrayExceptSelf.java
+++ Algorithms/src/amazon/ProductOfArrayExceptSelf.java
@@ -8,7 +8,7 @@ public class ProductOfArrayExceptSelf {
 		int[] retarr = fetchproductxcptself(arr);
 		
 		for(int i:retarr) {
-			System.out.println(i);
+			System.out.print(" "+i);
 		}
 	}
 
@@ -16,24 +16,30 @@ public class ProductOfArrayExceptSelf {
 		if(arr.length==0 || arr == null) {return null;}
 		
 		int[] output = new int[arr.length];
-		int[] helper = new int[arr.length];
+		
+		for(int i=0;i<arr.length;i++) {
+			output[i]=arr[i];
+		}
+		
 		int backmultiplier =1;
 		for(int i=arr.length-1;i>=0;i--) {
 			backmultiplier=backmultiplier*arr[i];
-			helper[i]=backmultiplier;
+			arr[i]=backmultiplier;
 		}
 		int forwardmultiplier =1;
-		output[0] = helper[1];
+		int temp=output[0];
+		output[0] = arr[1];
 		
 		for(int j=1;j<arr.length;j++) {
-			forwardmultiplier=forwardmultiplier*arr[j-1];
+			forwardmultiplier=forwardmultiplier*temp;
 			//System.out.println("fwdmltiplier  "+forwardmultiplier);
 			if(j==arr.length-1) {
 			output[j] = forwardmultiplier;
 			continue;
 		}
 			//System.out.println("helper "+helper[j+1]);
-			output[j] = forwardmultiplier*helper[j+1];	
+			temp=output[j];
+			output[j] = forwardmultiplier*arr[j+1];	
 		}
 		
 		
