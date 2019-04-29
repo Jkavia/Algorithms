c809b30d4105784941a999a7bf969f3c7ac5dcc0
Jkavia <Jkavia@github.com>
2019-04-29 16:24:13 -0400

    commit

---
+++ Algorithms/src/amazon/Subsets.java
@@ -0,0 +1,41 @@
+package amazon;
+import java.util.*;
+
+public class Subsets {
+
+	public static void main(String[] args) {
+	
+		int[] arr = {1,2,3};
+		List<List<Integer>> subsets = findSubsets(arr);
+		
+		for(List<Integer> ls:subsets) {
+			for(Integer in:ls) {
+				System.out.print(in);
+			}
+			System.out.println();
+		}
+		
+	}
+
+	private static List<List<Integer>> findSubsets(int[] arr) {
+			List<List<Integer>> retlist = new ArrayList<>();
+			
+			recursesubsets(retlist, new Stack<Integer>(), 0,arr);
+			
+			return retlist;
+	}
+
+	private static void recursesubsets(List<List<Integer>> retlist, Stack<Integer> stack, int position,int[] arr) {
+			if(arr.length==position) {
+				List<Integer> list = new ArrayList<>(stack);
+				retlist.add(list);
+				return;
+			}
+			
+			stack.add(arr[position]);
+			recursesubsets(retlist, stack, position+1,arr);
+			stack.pop();
+			recursesubsets(retlist, stack, position+1,arr);
+	}
+
+}
