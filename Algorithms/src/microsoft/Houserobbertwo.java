6798ff019abb3bf8a3274cb61426f294d9e6eb96
Jkavia <Jkavia@github.com>
2019-05-03 12:40:57 -0400

    commit

--- Algorithms/src/microsoft/Houserobbertwo.java
+++ Algorithms/src/microsoft/Houserobbertwo.java
@@ -3,11 +3,12 @@ package microsoft;
 public class Houserobbertwo {
 
 	public static void main(String[] args) {
-		int[] housesamout = {1,2,1,1};
+		int[] housesamout = { 1, 2, 1, 1 };
 		int max = findTheMaxProfit(housesamout);
 		System.out.println(max);
 
 	}
+
 	private static int findTheMaxProfit(int[] housesamout) {
 		if (housesamout == null) {
 			return 0;
@@ -18,21 +19,33 @@ public class Houserobbertwo {
 		if (housesamout.length == 2) {
 			return Math.max(housesamout[0], housesamout[1]);
 		}
+
+		int prev_r_rf = housesamout[0]
+		,prev_nr_rf =0,
+		prev_r_nrf = 0,
+		prev_nr_nrf =0;
 		
-	      int[] first = new int[housesamout.length+1];
-	        int[] second = new int[housesamout.length+1];
-	        
-	        first[0] = 0;
-	        first[1] = housesamout[0];
-	        second[0] = 0;
-	        second[1] = 0;
-	        
-	        
-	        for(int i =2; i<=housesamout.length ; i++){
-	            first[i] = Math.max(first[i-1], first[i-2]+housesamout[i-1]);
-	            second[i] = Math.max(second[i-1], second[i-2]+housesamout[i-1]);
-	        }
-	        
-			return Math.max(first[housesamout.length-1], second[housesamout.length]);
+		int curr_r_rf = housesamout[0]
+				,curr_nr_rf =0,
+				curr_r_nrf = 0,
+				curr_nr_nrf =0;
+		
+		
+
+		for (int i = 1; i <housesamout.length; i++) {
+			curr_r_rf = prev_nr_rf + housesamout[i];
+			curr_nr_rf = Math.max(prev_r_rf, prev_nr_rf);
+			curr_r_nrf = prev_nr_nrf+housesamout[i];
+			curr_nr_nrf =  Math.max(prev_nr_nrf, prev_r_nrf);
+			
+			
+			prev_r_rf = curr_r_rf;
+			prev_nr_rf =curr_nr_rf;
+			prev_r_nrf = curr_r_nrf;
+			prev_nr_nrf = curr_nr_nrf;
+			
+		}
+
+		return Math.max(Math.max(prev_r_nrf, prev_nr_rf), prev_nr_nrf);
 	}
 }
