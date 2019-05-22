205ac91060585f697e92ece49f7bb7aa17a2c692
Jkavia <Jkavia@github.com>
2019-05-21 22:35:03 -0400

    commit

--- Algorithms/src/microsoft/SpiralMatrix.java
+++ Algorithms/src/microsoft/SpiralMatrix.java
@@ -15,6 +15,50 @@ public class SpiralMatrix {
 	private static List<Integer> spiral(int[][] twodimmaxtrix) {
 		if(twodimmaxtrix==null && twodimmaxtrix.length==0) {return null;}
 		List<Integer> retlist = new ArrayList<Integer>();
+		
+		int imin=-1, jmin=-1, i=0,j=0;
+		int imax = twodimmaxtrix.length,jmax = twodimmaxtrix[0].length;
+		boolean right=true,down=false, left=false,up=false;
+		
+		
+		while(i>imin && i<imax && j>jmin && j<jmax) {
+			System.out.println(twodimmaxtrix[i][j]);
+			if(right) {
+				retlist.add(twodimmaxtrix[i][j]);
+				if(j==jmax-1) {
+					imin++;
+					down=true;right=false;}else {j++;}
+				
+			}
+			if(down) {
+				retlist.add(twodimmaxtrix[i][j]);
+				if(i==imax-1) {
+					jmax--;
+					left=true;down=false;}else {i++;}
+				
+			}
+			if(left) {
+				retlist.add(twodimmaxtrix[i][j]);
+				if(j==jmin-1) {
+					imax--;
+					up=true;left=false;}else {j--;}
+				
+			}
+			if(up) {
+				retlist.add(twodimmaxtrix[i][j]);
+				if(i==imin-1) {
+					jmin++;
+					right=true;up=false;}else {i--;}
+				
+			}
+			System.out.println(jmin+" "+imin+" "+jmax+" "+imax);
+			
+		}
+		
+		
+		
+		
+		
 		return retlist;
 	}
 
