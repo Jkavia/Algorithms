72a0d431d071f02f4027a03a1b35786d4bc30f02
Jkavia <Jkavia@github.com>
2019-05-22 23:45:51 -0400

    commit

--- Algorithms/src/microsoft/SpiralMatrix.java
+++ Algorithms/src/microsoft/SpiralMatrix.java
@@ -6,59 +6,44 @@ import java.util.List;
 public class SpiralMatrix {
 
 	public static void main(String[] args) {
-	int[][] twodimmaxtrix = {{1,2,3},{4,5,6},{7,8,9}};
+	int[][] twodimmaxtrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
 	
 	List<Integer> ls = spiral(twodimmaxtrix);
 
+	for(Integer in: ls) {
+		System.out.println(in);
+	}
 	}
 
 	private static List<Integer> spiral(int[][] twodimmaxtrix) {
 		if(twodimmaxtrix==null && twodimmaxtrix.length==0) {return null;}
 		List<Integer> retlist = new ArrayList<Integer>();
 		
-		int imin=-1, jmin=-1, i=0,j=0;
-		int imax = twodimmaxtrix.length,jmax = twodimmaxtrix[0].length;
-		boolean right=true,down=false, left=false,up=false;
+		int rowstart = 0, colstart =0, rowend = twodimmaxtrix.length-1, colend = twodimmaxtrix[0].length-1;
 		
-		
-		while(i>imin && i<imax && j>jmin && j<jmax) {
-			System.out.println(twodimmaxtrix[i][j]);
-			if(right) {
-				retlist.add(twodimmaxtrix[i][j]);
-				if(j==jmax-1) {
-					imin++;
-					down=true;right=false;}else {j++;}
-				
-			}
-			if(down) {
-				retlist.add(twodimmaxtrix[i][j]);
-				if(i==imax-1) {
-					jmax--;
-					left=true;down=false;}else {i++;}
-				
-			}
-			if(left) {
-				retlist.add(twodimmaxtrix[i][j]);
-				if(j==jmin-1) {
-					imax--;
-					up=true;left=false;}else {j--;}
-				
-			}
-			if(up) {
-				retlist.add(twodimmaxtrix[i][j]);
-				if(i==imin-1) {
-					jmin++;
-					right=true;up=false;}else {i--;}
-				
-			}
-			System.out.println(jmin+" "+imin+" "+jmax+" "+imax);
+		while(rowstart<=rowend && colstart<=colend) {
 			
+			for(int i=colstart; i<=colend; i++) {
+				retlist.add(twodimmaxtrix[rowstart][i]);
+			}rowstart++;
+			
+			for(int i=rowstart; i<=rowend; i++) {
+				retlist.add(twodimmaxtrix[i][colend]);
+			}colend--;
+			
+			if(rowstart<=rowend) {
+				for(int i=colend; i>=colstart; i--) {
+					retlist.add(twodimmaxtrix[rowend][i]);
+				}rowend--;
+			}
+			
+			if(colstart<=colend) {
+				for(int i=rowend; i>=rowstart; i--) {
+					retlist.add(twodimmaxtrix[i][colstart]);
+				}colstart++;
+			}
+
 		}
-		
-		
-		
-		
-		
 		return retlist;
 	}
 
