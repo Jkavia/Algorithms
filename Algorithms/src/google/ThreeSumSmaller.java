11389abc985d59c112e6baa55a0eda77aed0f596
Jkavia <Jkavia@github.com>
2019-05-23 23:08:12 -0400

    comment

--- Algorithms/src/google/ThreeSumSmaller.java
+++ Algorithms/src/google/ThreeSumSmaller.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class ThreeSumSmaller {
 
 	public static void main(String[] args) {
-		int[] input = {-2, 0, 1, 3};
+		int[] input = { -2, 0, 1, 3 };
 		int target = 2;
 
 		int ser = findsmaller(input, target);
@@ -17,26 +17,29 @@ public class ThreeSumSmaller {
 		}
 		int retnumber = 0;
 		Arrays.sort(input);
-		for(int i =0; i<input.length;i++) {
-		
-		
-			int index=i;
-			int j = index+1;
-			int k =input.length-1;
-			
-			while(j<k) {
-			
-				if((input[i]+input[j]+input[k])<target) {
-					retnumber+=k-j;//array is sorted so all number between j and k are smaller only
+		for (int i = 0; i < input.length; i++) {
+
+			int index = i;
+			int j = index + 1;
+			int k = input.length - 1;
+
+			while (j < k) {
+
+				if ((input[i] + input[j] + input[k]) < target) {
+					retnumber += k - j;// array is sorted so all number between j and k are smaller only
 					j++;
-					while(input[j]==input[j-1]) {j++;}
-				}else{
+					while (input[j] == input[j - 1]) {
+						j++;
+					}
+				} else {
 					k--;
-					while(input[k]==input[k+1]) {k--;}
+					while (input[k] == input[k + 1]) {
+						k--;
+					}
 				}
 			}
 		}
-		
+
 		return retnumber;
 	}
 
